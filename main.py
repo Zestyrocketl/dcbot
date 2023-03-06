50c0afe1dfeb862127856bd97e794343b7a2be62
Zestyrocketl <127130434+Zestyrocketl@users.noreply.github.com>
2023-03-06 20:12:50 +0100

    command1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from pathlib import Path
 
 import discord
 from discord.ext import commands
@@ -16,11 +17,19 @@ bot = commands.Bot(
     intents=discord.Intents.default(),
     activity=discord.Activity(type=discord.ActivityType.listening, name='Rocket League Music'),
     status=discord.Status.online,
-    sync_commands=False,
-    delete_not_existing_commands=False,
+    sync_commands=True,
+    delete_not_existing_commands=True,
 )
 
 if __name__ == '__main__':
     log.info('Starting Bot....'),
+    cogs = [p.stem for p in Path('cogs').glob('**/*.py') if not p.name.startswith('__')]
+    log.info('Loading %d extensions...', len(cogs))
+
+    for cog in cogs:
+        bot.load_extension(f'cogs.{cog}')
+        log.info('Loaded %s', cog)
+
+
     bot.run(os.getenv('BOT_TOKEN')),
 
